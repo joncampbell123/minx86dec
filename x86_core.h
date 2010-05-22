bd9c401d7049839740915e27bc6cb39415762271
jon <jon@18735561-da66-0410-a0b4-f50007a64df5>
2010-05-22 01:31:45 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/misc/minx86dec@222 18735561-da66-0410-a0b4-f50007a64df5

--- x86_core.h
+++ x86_core.h
@@ -3172,6 +3172,19 @@ decode_next:
 # endif
 
 # if core_level >= 5 && sse_level >= 2
+				case 0xEA: {
+					if (ins->rep >= MX86_REPE) {
+					}
+					else {
+						ARGV *rm = &ins->argv[1],*reg = &ins->argv[0];
+						ins->argc = 2; ins->opcode = MXOP_PMINSW; rm->size = reg->size = dataprefix32?16:8;
+						INS_MRM mrm = decode_rm_ex_(rm,ins,rm->size,PLUSR_TRANSFORM,dataprefix32?MX86_RT_SSE:MX86_RT_MMX);
+						if (dataprefix32) set_sse_register(reg,mrm.f.reg); else set_mmx_register(reg,mrm.f.reg);
+					}
+				} break;
+# endif
+
+# if core_level >= 5 && sse_level >= 2
 				COVER_2(0xEC): {
 					ARGV *d = &ins->argv[0],*s = &ins->argv[1]; d->size = s->size = dataprefix32?16:8;
 					ins->opcode = MXOP_PADDSB + (second_byte & 1); ins->argc = 2;
@@ -3352,24 +3365,6 @@ decode_next:
 							set_mmx_register(reg,mrm.f.reg);
 							decode_rm_ex(mrm,rm,isaddr32,MX86_RT_MMX);
 						}
-					}
-				} break;
-# endif
-
-# if core_level >= 5 && sse_level >= 2
-				case 0xEA: {
-					if (ins->rep >= MX86_REPE) {
-					}
-					else {
-						struct minx86dec_argv *rm = &ins->argv[1];
-						struct minx86dec_argv *reg = &ins->argv[0];
-						union x86_mrm mrm = fetch_modregrm();
-						ins->argc = 2;
-						ins->opcode = MXOP_PMINSW;
-						if (dataprefix32) set_sse_register(reg,mrm.f.reg);
-						else set_mmx_register(reg,mrm.f.reg);
-						rm->size = reg->size = dataprefix32 ? 16 : 8;
-						decode_rm_ex(mrm,rm,isaddr32,dataprefix32 ? MX86_RT_SSE : MX86_RT_MMX);
 					}
 				} break;
 # endif
