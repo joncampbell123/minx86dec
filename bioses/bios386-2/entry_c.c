daf270b1cbccfb58e9f72c5ebf83c4800fb052b2
jon <jon@18735561-da66-0410-a0b4-f50007a64df5>
2010-09-13 05:48:27 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/misc/minx86dec@304 18735561-da66-0410-a0b4-f50007a64df5

--- bioses/bios386-2/entry_c.c
+++ bioses/bios386-2/entry_c.c
@@ -1,24 +1,37 @@
 
 #include "x86_realmode.h"
 #include "bios_stdint.h"
-
-static inline void memw_b(const uint32_t addr,const uint8_t b) {
-#ifdef __NO_INLINE__
-	__asm__ __volatile__ (	"es\n"
-				"movb	%%al,(%%esi)"
-				: /* no outs */
-				: "a" ((unsigned int)b), "S" ((unsigned int)addr));
-#else
-	__asm__ __volatile__ (	"es\n"
-				"movb	%0,(%%esi)"
-				: /* no outs */
-				: "r" (b), "S" ((unsigned int)addr));
-#endif				
-}
+#include "bios_ioport.h"
+#include "bios_memio.h"
 
 /*=========================BIOS C ENTRY POINT==========================*/
 void __attribute__((noreturn)) _cpu_c_entry() {
+	unsigned int c;
+
+	iori_b(0x41);
+	c = ior_b(0x3CF);
+
+	iowi_b(0x41,0x22);
+	iow_b(0x3CF,0x44);
+
+	iori_w(0x41);
+	c = ior_w(0x3CF);
+
+	iowi_w(0x41,0x2222);
+	iow_w(0x3CF,0x4444);
+
+	iori_l(0x41);
+	c = ior_l(0x3CF);
+
+	iowi_l(0x41,0x22222222);
+	iow_l(0x3CF,0x44444444);
+
 	memw_b(0xB8000,'C');
 	memw_b(0xB8001,0x07);
+	c = memr_b(0xB8000);
+	memw_w(0xB8002,0x1234);
+	c = memr_w(0xB8002);
+	memw_l(0xB8004,0x12345678);
+	c = memr_l(0xB8004);
 }
 
