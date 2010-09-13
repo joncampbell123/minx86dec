a9108a482c45aea9c18ed756985f2bc2669f9185
jon <jon@18735561-da66-0410-a0b4-f50007a64df5>
2010-09-13 05:27:59 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/misc/minx86dec@303 18735561-da66-0410-a0b4-f50007a64df5

--- bioses/bios386-2/entry_c.c
+++ bioses/bios386-2/entry_c.c
@@ -2,16 +2,23 @@
 #include "x86_realmode.h"
 #include "bios_stdint.h"
 
-static inline void memw_b(uint32_t addr,uint8_t b) {
+static inline void memw_b(const uint32_t addr,const uint8_t b) {
+#ifdef __NO_INLINE__
 	__asm__ __volatile__ (	"es\n"
-				"movb	%0,%1"
-				:
-				: "r" (b), "m" (addr));
-				
+				"movb	%%al,(%%esi)"
+				: /* no outs */
+				: "a" ((unsigned int)b), "S" ((unsigned int)addr));
+#else
+	__asm__ __volatile__ (	"es\n"
+				"movb	%0,(%%esi)"
+				: /* no outs */
+				: "r" (b), "S" ((unsigned int)addr));
+#endif				
 }
 
 /*=========================BIOS C ENTRY POINT==========================*/
 void __attribute__((noreturn)) _cpu_c_entry() {
 	memw_b(0xB8000,'C');
+	memw_b(0xB8001,0x07);
 }
 
