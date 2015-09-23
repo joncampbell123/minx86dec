5719b53b015e3e97d667023cbddb5f0469dede9b
Jonathan Campbell <jonathan@castus.tv>
2015-09-23 16:03:17 -0700

    more

--- git-push-to-github.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git remote add github https://joncampbell123@github.com/joncampbell123/minx86dec.git # might fail
-git remote set-url github https://joncampbell123@github.com/joncampbell123/minx86dec.git # might fail
-git push github master
-
